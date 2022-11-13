f58498cf7b7db97d24dc9e53e8e3f17245570006
Geoff Mendoza <geoffreymendoza.dev@gmail.com>
2022-11-13 11:59:10 +0800

    -added revision number variable

--- Assets/_scripts/TestScript.cs
+++ Assets/_scripts/TestScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    [SerializeField] private int _revisionNumber = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
