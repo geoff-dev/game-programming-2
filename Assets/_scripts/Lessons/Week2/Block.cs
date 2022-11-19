1da2a295e0ead5a3e2e9b25add2ccc7a01c60652
Geoff Mendoza <geoffreymendoza.dev@gmail.com>
2022-11-19 12:31:52 +0800

    -updated week 2 lessons

---
+++ Assets/_scripts/Lessons/Week2/Block.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GP2.Lessons
+{
+    public class Block : MonoBehaviour
+    {
+        public string Name;
+        public int Health;
+    
+    }
+
+}
